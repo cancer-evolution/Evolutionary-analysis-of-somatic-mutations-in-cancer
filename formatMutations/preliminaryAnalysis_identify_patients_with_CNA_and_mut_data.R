ab71bcc41ee920e94b082c1df5620777a910f2f7
cancer-evolution <anna.trigos@petermac.org>
2019-01-17 10:56:24 +1100

    Update preliminaryAnalysis_identify_patients_with_CNA_and_mut_data.R
    Analyses including the entire TCGA cohort

--- formatMutations/preliminaryAnalysis_identify_patients_with_CNA_and_mut_data.R
+++ formatMutations/preliminaryAnalysis_identify_patients_with_CNA_and_mut_data.R
@@ -1,10 +1,14 @@
 ###Obtain background list of patients with data on CNVs and somatic mutations
 
-tumours <- c("LUAD", "LUSC", "BRCA", "PRAD", "LIHC", "COAD", "STAD")
+tumours <- c("ACC", "BLCA", "BRCA", "CESC", "CHOL", "COAD", "ESCA", "GBM", 
+             "HNSC", "KICH", "KIRC", "KIRP", "LGG",  "LIHC", "LUAD", "LUSC", 
+             "OV", "PAAD", "PCPG", "PRAD", "READ", "SARC", "SKCM", "STAD", 
+             "TGCT", "THCA", "THYM", "UCEC", "UCS", "UVM")
 
 patients_with_mut_info <- list()
 for(tumour in tumours){
-  filenames <- list.files(pattern="*.maf", full.names=TRUE)  ##MAF files from TCGA
+  path <- ###Path to maf files  
+  filenames <- list.files(path, pattern="*.maf$", full.names=TRUE)  ##MAF files from TCGA
   maf_files <- lapply(filenames, read.delim, comment.char = "#")
   names(maf_files) <- c("muse", "mutect", "somaticsniper", "varscan")
   patients <- unique(c(as.character(maf_files$muse$Tumor_Sample_Barcode),
@@ -13,27 +17,33 @@ for(tumour in tumours){
                        as.character(maf_files$varscan$Tumor_Sample_Barcode)))
   patients <- substr(patients, 9, 12)
   patients_with_mut_info[[tumour]] <- patients
+  print(tumour)
 }
 save(patients_with_mut_info, file="patients_with_mut_info.Rdata")
+
+sum(sapply(patients_with_mut_info, length))
 
 
 patients_with_CNV_info <- list()
 for(tumour in tumours){
-  TCGA_files <- read.table(paste("gdac.broadinstitute.org_", tumour, ".Merge_snp__genome_wide_snp_6__broad_mit_edu__Level_3__segmented_scna_minus_germline_cnv_hg19__seg.Level_3.2016012800.0.0/", tumour, ".snp__genome_wide_snp_6__broad_mit_edu__Level_3__segmented_scna_minus_germline_cnv_hg19__seg.seg.txt", sep=""),
-                           header=TRUE)
-  
+  TCGA_files <- read.table(paste(tumour, "/gdac.broadinstitute.org_", tumour, ".Merge_snp__genome_wide_snp_6__broad_mit_edu__Level_3__segmented_scna_minus_germline_cnv_hg19__seg.Level_3.2016012800.0.0/", tumour, ".snp__genome_wide_snp_6__broad_mit_edu__Level_3__segmented_scna_minus_germline_cnv_hg19__seg.seg.txt", sep=""),
+                           header=TRUE)  #File from TCGA
+
   #Select tumour samples
   samples <- TCGA_files$Sample
   sample_codes <- substring(samples, 14, 16)
   
   #Sample types available: "10A" "01A" "11A" "11B" "01B" "10B" "06A"
   
-  normal_samples <- which(sample_codes %in% c("11A", "11B"))
-  tumour_samples <- which(sample_codes %in% c("01A", "01B"))
+  #normal_samples <- which(sample_codes %in% c("11A", "11B"))
+  tumour_samples <- which(sample_codes %in% c("01A", "01B", "01C", "01D"))
   
-  TCGA_files <- TCGA_files[c(tumour_samples, normal_samples),]
+  TCGA_files <- TCGA_files[c(tumour_samples),]
   patients <- unique(substr(TCGA_files$Sample, 9, 12))
   patients_with_CNV_info[[tumour]] <- patients
+  print(tumour)
 }
 save(patients_with_CNV_info, file="patients_with_CNV_info.Rdata")
 
+load("patients_with_CNV_info.Rdata")
+sum(sapply(patients_with_CNV_info, length))
